94f45bacd1ba287e28fb710b8b9c740a7db3dfc6
edgarshmavonyan <shmavonyan.eha@phystech.edu>
2018-05-11 16:38:47 +0300

    main func

--- run_bot.py
+++ run_bot.py
@@ -1,23 +1,25 @@
 from controller.queries import *
 from telegram.ext import CommandHandler, Updater
 
-updater = Updater(token='585285364:AAFBx3Kotx7txqpBN03RXYHHm_e1ViGqNjY',
-                  request_kwargs={'proxy_url': 'socks5://138.68.98.172:1080/'}
-                  )
-dispatcher = updater.dispatcher
-import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-new_docs_handler = CommandHandler('new_docs', NewDocsHandler.handle, pass_args=True)
-dispatcher.add_handler(new_docs_handler)
 
-new_topics_handler = CommandHandler('new_topics', NewTopicsHandler.handle, pass_args=True)
-dispatcher.add_handler(new_topics_handler)
+def main():
+    updater = Updater(token='585285364:AAFBx3Kotx7txqpBN03RXYHHm_e1ViGqNjY',
+                      request_kwargs={'proxy_url': 'socks5://138.68.98.172:1080/'}
+                      )
+    dispatcher = updater.dispatcher
+    import logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    new_docs_handler = CommandHandler('new_docs', NewDocsHandler.handle, pass_args=True)
+    dispatcher.add_handler(new_docs_handler)
 
-topic_handler = CommandHandler('topic', TopicHandler.handle, pass_args=True)
-dispatcher.add_handler(topic_handler)
+    new_topics_handler = CommandHandler('new_topics', NewTopicsHandler.handle, pass_args=True)
+    dispatcher.add_handler(new_topics_handler)
 
-doc_handler = CommandHandler('doc', DocHandler.handle, pass_args=True)
-dispatcher.add_handler(doc_handler)
+    topic_handler = CommandHandler('topic', TopicHandler.handle, pass_args=True)
+    dispatcher.add_handler(topic_handler)
 
-updater.start_polling()
+    doc_handler = CommandHandler('doc', DocHandler.handle, pass_args=True)
+    dispatcher.add_handler(doc_handler)
+
+    updater.start_polling()
